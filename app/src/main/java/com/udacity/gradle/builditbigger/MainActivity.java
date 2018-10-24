c58d086f711ee26d66a8895d8b326cc3ad6af67a
Ahmed Hesham <eng.ahmed.86313@gmail.com>
2018-10-24 04:30:32 +0200

    Step 2: Create an Android Library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.androidlibjokes.JokeActivity;
 import com.example.javajokes.Jokes;
 
 
@@ -44,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
         Jokes jokes = new Jokes();
         Toast.makeText(this, jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokeActivity.class);
+        startActivity(intent);
     }
 
 
